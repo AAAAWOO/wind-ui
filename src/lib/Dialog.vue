285f5c182903428cddb577e8c6a1e1e07261a950
AAAAWOO <bandingeye@163.com>
2025-05-11 00:50:11 +0800

    Dialog 支持自定义 title 和 content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="wind-dialog-wrapper">
         <div class="wind-dialog">
           <header>
-            标题
+            <slot name="title" />
             <span @click="close" class="wind-dialog-close"></span>
           </header>
           <main>
-            <p>第一行字</p>
-            <p>第二行字</p>
+            <slot name="content" />
           </main>
           <footer>
             <Button level="main" @click="ok">OK</Button>

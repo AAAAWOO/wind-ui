629025b4d73c8f0cb6266eed3594862c1502b0cc
AAAAWOO <bandingeye@163.com>
2025-05-11 01:15:48 +0800

    使用 CSS 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,8 +4,7 @@
       <div class="wind-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
     </div>
     <div class="wind-tabs-content">
-      {{current}}
-      <component class="wind-tabs-content-item" :is="current" />
+      <component class="wind-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
     </div>
 </div>
 </template>
@@ -73,6 +72,12 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>

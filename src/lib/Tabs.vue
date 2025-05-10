25752b6558fd9e607032cb540ebd4b57fc0b2d2a
AAAAWOO <bandingeye@163.com>
2025-05-11 01:06:26 +0800

    Tabs css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
-  </div>
+<div class="wind-tabs">
+    <div class="wind-tabs-nav">
+        <div class="wind-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="wind-tabs-content">
+        <component class="wind-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -25,3 +29,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.wind-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>

16d67a2d47ea8f5c18da67729126f5fbdb837eb5
AAAAWOO <bandingeye@163.com>
2025-05-11 01:30:34 +0800

    优化3

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -15,8 +15,6 @@ import Tab from './Tab.vue'
 import {
   computed,
   ref,
-  onMounted,
-  onUpdated,
   watchEffect
 } from 'vue'
 
@@ -51,11 +49,7 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-    const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
-    })
+     
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
@@ -65,7 +59,6 @@ export default {
     return {
       defaults,
       titles,
-      current,
       select,
       selectedItem,
       indicator,
